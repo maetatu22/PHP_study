01a65a58531edc13dfb5a330e33a0dd2924935b9
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-23 00:23:20 +0900

    連想配列

--- practice3/index.php
+++ practice3/index.php
@@ -14,5 +14,15 @@
     echo $colors[3];
   ?>
 
+  <?php
+    $scores = array(
+                '数学' => 70,
+                '英語' => 90,
+                '国語' => 80
+              );
+    $scores["国語"] += 5;
+    echo $scores['国語'];
+  ?>
+
 </body>
 </html>
