242d16beefd5592a9ae4a5adab680bab78089131
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-22 23:53:56 +0900

    if分　比較演算子

---
+++ practice2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <?php
+    $x = 99 * 99;
+    $y = 77 * 77;
+
+    if($x > 9800){
+      echo '変数xは9800より大きいです。';
+    }
+    if($y > 6800){
+      echo '変数yは6000より大きいです。';
+    }
+  ?>
+</body>
+</html>
