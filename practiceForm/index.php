3d6a5613e33cd8c2ca1600faa8bfb1da58bf3cd7
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-24 00:50:18 +0900

    お問い合わせフォーム

--- practiceForm/index.php
+++ practiceForm/index.php
@@ -12,7 +12,7 @@
       <ul>
         <li>会社概要</li>
         <li>採用</li>
-        <li class="selected"> お問い合わせ</li>
+        <li class="selected">お問い合わせ</li>
       </ul>
     </div>
   </div>
@@ -20,27 +20,43 @@
   <div class="main">
     <div class="contact-form">
       <div class="form-title">お問い合わせ</div>
-
-      <form action="sent.php" method="post">
-
+      <form method="post" action="sent.php">
         <div class="form-item">名前</div>
         <input type="text" name="name">
 
         <div class="form-item">年齢</div>
         <select name="age">
           <option value="未選択">選択してください</option>
-          <option value="20代">20代</option>
-          <option value="30代">30代</option>
+          
+          <?php 
+            for($i = 6; $i <= 100; $i++){
+              echo "<option value='{$i}'>{$i}</option>";
+            }
+          ?>
         </select>
 
+        <div class="form-item">お問い合わせの種類</div>
+        <?php 
+          $types = array('Progateに関するお問い合わせ', 'Progateに対する意見', '採用に関するお問い合わせ', '取材・メディア関連のお問い合わせ', '料金に関するお問い合わせ', 'その他');
+         ?>
+        <select name="category">
+          <option value="未選択">選択してください</option>
+          <?php
+            foreach ($types as $type){
+              echo "<option value='{$type}'>{$type}</option>";
+            }
+          ?>
+        </select>
+        
+        
         <div class="form-item">内容</div>
         <textarea name="body"></textarea>
+
         <input type="submit" value="送信">
       </form>
-
     </div>
   </div>
-
+  
   <div class="footer">
     <div class="footer-left">
       <ul>
@@ -50,7 +66,7 @@
       </ul>
     </div>
     <div class="like-box">
-    <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
+      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
     </div>
   </div>
 </body>
--- practiceForm/sent.php
+++ practiceForm/sent.php
@@ -20,26 +20,20 @@
   <div class="main">
     <div class="thanks-message">お問い合わせいただきありがとうございます。</div>
     <div class="display-contact">
-
       <div class="form-title">入力内容</div>
 
       <div class="form-item">■ 名前</div>
-      <?php
-      echo $_POST['name'];
-      ?>
+      <?php echo $_POST['name']; ?>
 
       <div class="form-item">■ 年齢</div>
-      <?php
-      echo $_POST['age'];
-      ?>
+      <?php echo $_POST['age']; ?>
+
+      <div class="form-item">■ お問い合わせの種類</div>
+      <?php echo $_POST['category']; ?>
       
       <div class="form-item">■ 内容</div>
-      <?php
-      echo $_POST['body'];
-      ?>
-      
+      <?php echo $_POST['body']; ?>
     </div>
-
   </div>
 
   <div class="footer">
@@ -51,7 +45,7 @@
       </ul>
     </div>
     <div class="like-box">
-      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
+      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
     </div>
   </div>
 </body>
