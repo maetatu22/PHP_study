2d72a4931adee770da0615902bb0ad3514c42f71
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-25 19:09:14 +0900

    最高金額出力

--- exam1/index8.php
+++ exam1/index8.php
@@ -6,9 +6,16 @@
   );
 
   $totalPrice = 0;
+  $maxPrice = 0;
+  $maxMenu = "";
   foreach ($menus as $menu){
    echo $menu['name']."は".$menu['price']."円です"."<br>";
    $totalPrice += $menu['price'];
+   if($maxPrice < $menu['price']){
+     $maxPrice = $menu['price'];
+     $maxMenu = $menu['name'];
+   }
   };
-  echo "合計金額は".$totalPrice."円です";
+  echo "合計金額は".$totalPrice."円です"."<br>";
+  echo $maxMenu."が最高価格で".$maxPrice."円です";
 ?>
