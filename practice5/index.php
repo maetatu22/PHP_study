15d1fe1717be960634ff92acadda52fdd21f7f32
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-26 20:38:42 +0900

    プロパティ

--- practice5/index.php
+++ practice5/index.php
@@ -1,8 +1,15 @@
 <?php
   class Menu{
-
+    public $name;
   }
 
   $curry = new Menu();
   $pasta = new Menu();
+
+  $curry->name = "CURRY";
+  $pasta->name = "PASTA";
+
+  echo '<br>';
+  echo $pasta->name."<br>";
+  echo $curry->name;
 ?>
