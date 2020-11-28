c9b94e023688878f9860e6779b78be530c04c035
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-28 20:17:08 +0900

    コンストラクタ　引数

---
+++ practice5/index4.php
@@ -0,0 +1,26 @@
+<?php
+class Menu {
+  public $name;
+  
+
+  public function __construct($name) {
+
+    $this->name = $name;
+    
+  }
+  
+  public function hello() {
+    echo '私は'.$this->name.'です';
+  }
+}
+
+
+$curry = new Menu("CURRY");
+
+$pasta = new Menu("PASTA");
+
+$curry->hello();
+echo '<br>';
+$pasta->hello();
+
+?>
