54b3f5dac9dc6113a91c017350611e06ca21ac17
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-29 22:53:06 +0900

    メニュー　名前一覧表示

--- practice6/index.php
+++ practice6/index.php
@@ -16,21 +16,25 @@
   $curry = new Menu('CURRY');
   $pasta = new Menu('PASTA');
 
+  $menus = array($juice, $coffee, $curry, $pasta);
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Café Progate</title>
+  <title>Cafe Progate</title>
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
   <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
 </head>
 <body>
   <div class="menu-wrapper container">
     <h1 class="logo">Café Progate</h1>
-    <div class="menu-items">
-     
+    <div class="menu-items">  
+    <?php foreach($menus as $menu): ?>
+      <h3><?php echo $menu->name ?></h3>
+    <?php endforeach ?>
       
       
     </div>
