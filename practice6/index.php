97653350c194c1e33606a9d6efd7eba01074a921
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-29 22:05:17 +0900

    雛形

---
+++ practice6/index.php
@@ -0,0 +1,18 @@
+<?php
+  class Menu{
+    public $name:
+
+    public function __construct($name){
+      $this->name = $name;
+    }
+
+    public function hello(){
+      echo '私は'.$this->name.'です';
+    }
+  }
+
+  $curry = new Menu('CURRY');
+  $pasta = new Menu('PASTA');
+
+?>
+
