8d29d84cfca1c45b4075ba0ca96a7f661ba7a24b
maetatu22 <zbn.ex.zz@gmail.com>
2020-12-07 14:31:07 +0900

    オーバーライド

--- Object-orientation/drink.php
+++ Object-orientation/drink.php
@@ -3,6 +3,15 @@ require_once('menu.php');
 
 class Drink extends Menu {
   private $type;
+
+  public function __construct($name, $price, $image, $type) {
+    $this->name = $name;
+    $this->price = $price;
+    $this->image = $image;
+    $this->type = $type;
+    self::$count++;
+    
+  }
   
   public function getType() {
     return $this->type;
